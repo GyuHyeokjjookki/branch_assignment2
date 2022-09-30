28928cde5170c42001bd5d0731ec32819778df5a
gyu_hyeok <jjookki33@gmail.com>
2022-09-30 15:59:11 +0900

    gitassignment2.cpp modifyied

--- assignment2.cpp
+++ assignment2.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main() {
 	string str;
+	cout << "Enter a string : ";
 	cin >> str;
-	cout << str << endl;
-
+	if (str == "exit") {
+		return 0;
+	}
+	else {
+		cout << "Your result : " << str << endl;
+	}
+	
 	return 0;
 }
